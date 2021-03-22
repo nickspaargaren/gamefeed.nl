c7a5d489c5f6296ad4d942ccfd9859e6e3c16db4
Nick Spaargaren <nickspaargaren@users.noreply.github.com>
2021-03-22 18:50:02 +0100

    Channel toegevoegd

---
+++ data/channels.js
@@ -0,0 +1,6 @@
+const channels = [
+  {id: "UCjBp_7RuDBUYbd1LegWEJ8g", naam: "xbox"},
+  {id: "UC-2Y8dQb0S6DtpxNgAKoJKA", naam: "playstation"}
+]
+
+export default channels
--- pages/api/update.js
+++ pages/api/update.js
@@ -1,5 +1,6 @@
 
 import { server } from '../../config';
+import channels from '../../data/channels'
 const fetch = require('node-fetch');
 var MongoClient = require('mongodb').MongoClient
 
@@ -10,30 +11,31 @@ const update = (req, res) => {
 
     var db = client.db('main')
 
+    channels.map((channel) => {
 
-    fetch(`https://www.googleapis.com/youtube/v3/search?key=${process.env.APIKEYGOOGLE}&channelId=UCjBp_7RuDBUYbd1LegWEJ8g&part=snippet,id&order=date&maxResults=20`)
-    .then(response => response.json())
-    .then(data => {
-      // console.log(data)
+      fetch(`https://www.googleapis.com/youtube/v3/search?key=${process.env.APIKEYGOOGLE}&channelId=${channel.id}&part=snippet,id&order=date&maxResults=20`)
+      .then(response => response.json())
+      .then(data => {
+        // console.log(data)
 
-      data.items.map((item, index) =>  { 
-        const query = { title: item.snippet.title };
-        const update = { $set: { title: item.snippet.title, description: item.snippet.description, etag: data.etag, image: item.snippet.thumbnails.default.url, videoId: item.id.videoId, channel: item.snippet.channelTitle, publishedAt: item.snippet.publishedAt }};
-        const options = { upsert: true };
+        data.items.map((item, index) =>  { 
+          const query = { title: item.snippet.title };
+          const update = { $set: { title: item.snippet.title, description: item.snippet.description, etag: data.etag, image: item.snippet.thumbnails.default.url, videoId: item.id.videoId, channel: item.snippet.channelTitle, publishedAt: item.snippet.publishedAt }};
+          const options = { upsert: true };
 
-        return (
-          db.collection('feed').updateOne(query, update, options)
+          return (
+            db.collection('feed').updateOne(query, update, options)
+          )
+        })
+
+        // log updaten
+        db.collection('log').insertOne(
+          { feed: channel.naam, etag: "BTMUwOpUrva3_ln-f9Sp3dzpLiY", ts: Date.now(), server: `${server}` }
         )
+        
       })
-
-
-      // log updaten
-      db.collection('log').insertOne(
-        { feed: "xbox", etag: "BTMUwOpUrva3_ln-f9Sp3dzpLiY", ts: Date.now(), server: `${server}` }
-      )
-      
+      .catch(err => console.log(err))
     })
-    .catch(err => console.log(err))
 
   })
   res.status(200).json({ status: 'Updated' })
